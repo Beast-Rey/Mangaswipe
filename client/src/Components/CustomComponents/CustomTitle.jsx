e2a8cf8773eec8f4ced52cc0405aab9c8ec86518
Beast-Rey <bemine.arun74@gmail.com>
2023-08-01 19:26:28 +0530

    updated recentupdates manga

---
+++ client/src/Components/CustomComponents/CustomTitle.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export default function CustomTitle({ title }) {
+    return (
+        <div className="flex justify-between">
+            <h1 className="mangaHead">
+                {title} <span>&#187;</span>
+            </h1>
+            <Link to="/recent">
+                <span className="underline decoration-manga-yellow decoration-2 underline-offset-8 cursor-pointer">
+                    View All
+                </span>
+            </Link>
+        </div>
+    )
+}
--- client/src/Components/RecentUpdates/RecentUpdates.jsx
+++ client/src/Components/RecentUpdates/RecentUpdates.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import RecentUpdateGrid from "./RecentUpdateGrid";
 import SkeletonGrid from "../../Skeleton/SkeletonGrid";
+import CustomTitle from "../CustomComponents/CustomTitle";
 
 const RecentlyUpdate = () => {
   let id;
@@ -19,18 +20,9 @@ const RecentlyUpdate = () => {
   }
 
   return (
-    <section className="w-[100%] mx-auto lg:w-[85%] xl:w-[69%] my-10">
-      <div className="w-[90%] mx-auto lg:w-[100%] flex justify-between">
-        <h1 className="mangaHead">
-          Recently Update Reads <span>&#187;</span>
-        </h1>
-        <Link to="/recent">
-          <span className="underline decoration-[#EFC416] decoration-2 underline-offset-8 cursor-pointer">
-            View All
-          </span>
-        </Link>
-      </div>
-      <div className="pt-1 lg:pt-0 text-black dark:text-white mt-10 bg-[#F0F9FA] dark:bg-[#282F30] md:flex md:w-[90%] mx-auto rounded-lg lg:w-[52.5rem] xl:w-[100%]">
+    <section className="container mx-auto my-10">
+     <CustomTitle title={'Recently Update Reads'}/>
+      <div className="pt-1 lg:pt-0 text-black dark:text-white mt-10 bg-[#F0F9FA] dark:bg-[#282F30] md:flex md:w-[100%] mx-auto rounded-lg lg:w-[52.5rem] xl:w-[100%]">
         <div className="w-[90%] md:w-[50%] h-[516px] text-center space-y-3 rounded-lg m-4 bg-[#FFFFFF] dark:bg-[#131415] pt-[1.25rem]">
           {recent &&
             recent.map((listData, index) => {
